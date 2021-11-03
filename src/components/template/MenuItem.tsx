17f176085f93b39308f6ed789d86682372de1042
Gabriel Sobreira <gabriellaranjasobreira@gmail.com>
2021-11-03 14:09:54 -0300

    Sidebar finalizada

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -1,26 +1,29 @@
 import Link from 'next/link';
 
 interface MenuItemProps {
-  url: string;
+  url?: string;
   text: string;
   icon: any;
+  onClick?: (evento: any) => void;
+  className?: string;
 }
 
 export const MenuItem = (props: MenuItemProps) => {
+  const renderLink = () => {
+    return (
+      <a
+        className={`
+            flex flex-col justify-center items-center h-20 w-20 text-gray-600 ${props.className}
+            `}
+      >
+        {props.icon}
+        <span className={`text-xs font-light `}>{props.text}</span>
+      </a>
+    );
+  };
   return (
-    <li className={`hover:bg-gray-100`}>
-      <Link href={props.url}>
-        <a
-          className={`
-        flex flex-col justify-center items-center h-20 w-20
-        `}
-        >
-          {props.icon}
-          <span className={`text-xs font-light text-gray-600`}>
-            {props.text}
-          </span>
-        </a>
-      </Link>
+    <li className={`hover:bg-gray-100 cursor-pointer`} onClick={props.onClick}>
+      {props.url ? <Link href={props.url}>{renderLink()}</Link> : renderLink()}
     </li>
   );
 };
--- src/components/template/Sidebar.tsx
+++ src/components/template/Sidebar.tsx
@@ -1,22 +1,34 @@
-import { BellIcon, HomeIcon, SettingsIcon } from '../icons';
+import { BellIcon, HomeIcon, LogoutIcon, SettingsIcon } from '../icons';
 import { Logo } from './Logo';
 import { MenuItem } from './MenuItem';
 
 export const Sidebar = () => {
   return (
-    <aside>
+    <aside className="flex flex-col">
       <div
-        className={`flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-800 h-20 w-20`}
+        className={`
+        flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-800 h-20 w-20`}
       >
         <Logo />
       </div>
-      <ul>
+      <ul className="flex-grow ">
         <MenuItem url="/" text="Início" icon={HomeIcon}></MenuItem>
         <MenuItem url="/ajustes" text="Ajustes" icon={SettingsIcon}></MenuItem>
         <MenuItem
           url="/notificacoes"
           text="Notificações"
           icon={BellIcon}
+        ></MenuItem>
+      </ul>
+      <ul>
+        <MenuItem
+          onClick={() => console.log('logout')}
+          text="Sair"
+          icon={LogoutIcon}
+          className={`
+            text-red-600
+            hover:bg-red-400 hover:text-white
+          `}
         ></MenuItem>
       </ul>
     </aside>
