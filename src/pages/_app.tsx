39ab7543978f6141ca58ef5427dcefeab60a8240
Gabriel Sobreira <gabriellaranjasobreira@gmail.com>
2021-11-03 17:22:45 -0300

    Correção do nome da pasta context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
-import { AppProvider } from '../data/content/AppContext';
+import { AppProvider } from '../data/context/AppContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
